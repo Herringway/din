b0ee4b8039365823b08b71e8506d6a93b3bedb7c
Cameron Ross <elpenguino@gmail.com>
2014-02-26 13:21:42 -0400

    add toasty support. (WARNING: UNTESTED)

---
+++ source/toasty.d
@@ -0,0 +1,30 @@
+module toasty;
+
+private import std.uri;
+private import std.string;
+private import std.net.curl;
+private import notifier;
+
+
+class toasty : Notifier {
+	private string[] targets;
+	private string _APIKey;
+	@property string APIKey(string key) {
+		return _APIKey = key;
+	}
+	void setTargets(string[] targs) {
+		targets = targs;
+	}
+	void send(notification toSend) {
+		auto client = HTTP();
+		client.verifyPeer(false);
+		client.addRequestHeader("Content-Type", "multipart/form-data");
+		foreach (id; targets) {
+			auto postData = ["title":toSend.title, "sender":toSend.apptitle, "text":toSend.message];
+			post(format("http://api.supertoasty.com/notify/%s", id), format("%(%(%c%)=%(%c%)&%)", postData).encode(), client);
+		}
+	}
+	@property bool needsAPIKey() {
+		return false;
+	}
+}
