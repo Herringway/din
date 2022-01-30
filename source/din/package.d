423c4da8ca51863ea05a7c438befcd6142c39afe
Cameron Ross <elpenguino@gmail.com>
2022-01-30 02:56:52 -0400

    remove dead services

--- source/din/package.d
+++ source/din/package.d
@@ -1,7 +1,4 @@
 module din;
 public import din.api;
-public import din.notifiers.faast;
-public import din.notifiers.notifymyandroid;
 public import din.notifiers.prowl;
 public import din.notifiers.pushover;
-public import din.notifiers.toasty;
